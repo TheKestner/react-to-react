05453115d883c94d2569716363fbb5a1980dd6df
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-03-15 17:24:51 +0000

    updated readme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Link to="/houses">Houses</Link> |{" "}
         <Link to="/elixirs">Elixirs</Link> |{" "}
         <Link to="/ingredients">Ingredients</Link> |{" "}
-        <Link to="/wizards">Wizards</Link>
+        <Link to="/wizards">Wizards</Link> |{" "}
       </nav>
       <Outlet />
     </div>
